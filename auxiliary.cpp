ad1bc801ed1b89240ae0b79972cd38f7de5b39a3
ynilu <michael142536@gmail.com>
2016-05-28 23:04:41 +0800

    finish constructor of aux_graph

--- auxiliary.cpp
+++ auxiliary.cpp
@@ -2,9 +2,11 @@
 
 using namespace std;
 
-Aux_node::Aux_node(int id)
+Aux_node::Aux_node(int id, int phy_id, Aux_node_type type)
 {
     this -> id = id;
+    this -> phy_id = phy_id;
+    this -> type = type;
     first_in = NULL;
     last_in = NULL;
     first_out = NULL;
@@ -16,7 +18,7 @@ Aux_node::~Aux_node()
     Aux_link *current;
     Aux_link *next;
 
-    // remove all the in links
+    // remove all the incoming links
     current = this -> first_in;
     while(current != NULL)
     {
@@ -25,7 +27,7 @@ Aux_node::~Aux_node()
         current = next;
     }
 
-    // remove all the out links
+    // remove all the outgoing links
     current = this -> first_out;
     while(current != NULL)
     {
@@ -35,14 +37,14 @@ Aux_node::~Aux_node()
     }
 }
 
-Aux_link::Aux_link(Aux_node* from, Aux_node* to, float cost)
+Aux_link::Aux_link(Aux_node* from, Aux_node* to, double weight, Aux_link_type type)
 {
-    this -> cost = cost;
-
     this -> from = from;
     this -> to = to;
+    this -> weight = weight;
+    this -> type = type;
 
-    // same from list
+    // same_from list
     Aux_link *from_last_out = from -> last_out;
     if(from_last_out == NULL) // list is empty;
     {
@@ -59,7 +61,7 @@ Aux_link::Aux_link(Aux_node* from, Aux_node* to, float cost)
         from -> last_out = this;
     }
 
-    // same to list
+    // same_to list
     Aux_link *to_last_in = to -> last_in;
     if(to_last_in == NULL) // list is empty;
     {
@@ -82,7 +84,7 @@ Aux_link::~Aux_link()
     Aux_link *prev;
     Aux_link *next;
 
-    // same_from list
+    // maintain same_from list
     prev = this -> prev_same_from;
     next = this -> next_same_from;
 
@@ -107,7 +109,7 @@ Aux_link::~Aux_link()
         next -> prev_same_from = prev;
     }
 
-    // same_to list
+    // maintain same_to list
     prev = this -> prev_same_to;
     next = this -> next_same_to;
 
@@ -130,5 +132,45 @@ Aux_link::~Aux_link()
     {
         prev -> next_same_to = next;
         next -> prev_same_to = prev;
+    }
+
+}
+
+Aux_graph::Aux_graph(int num_phy_node, int num_slots)
+{
+
+    // reserve space for all possible auxiliary nodes
+    aux_node.reserve((num_slots + 2) * num_phy_node);
+
+    Aux_node* a_node;   // adding node
+    Aux_node* d_node;   // dropping node
+    Aux_node* v_t_node; // virtual transmitting node
+    Aux_node* v_r_node; // virtual receiving node
+
+    for(int phy_node_i = 0; phy_node_i < num_phy_node; phy_node_i++)
+    {
+        a_node   = new Aux_node(phy_node_i * 4 + 0, phy_node_i, Aux_node::adding_node);
+        d_node   = new Aux_node(phy_node_i * 4 + 1, phy_node_i, Aux_node::dropping_node);
+        v_t_node = new Aux_node(phy_node_i * 4 + 2, phy_node_i, Aux_node::virtual_transmitting_node);
+        v_r_node = new Aux_node(phy_node_i * 4 + 3, phy_node_i, Aux_node::virtual_receiving_node);
+
+        aux_node.push_back(a_node);
+        aux_node.push_back(d_node);
+        aux_node.push_back(v_t_node);
+        aux_node.push_back(v_r_node);
+
+        new Aux_link(d_node, a_node, 1, Aux_link::grooming_link);
+        new Aux_link(a_node, v_t_node, 1, Aux_link::virtual_adding_link);
+        new Aux_link(v_r_node, d_node, 1, Aux_link::virtual_dropping_link);
+    }
+
+}
+
+Aux_graph::~Aux_graph()
+{
+    while (!aux_node.empty())
+    {
+        delete aux_node.back();
+        aux_node.pop_back();
     }
 }
--- auxiliary.h
+++ auxiliary.h
@@ -2,35 +2,63 @@
 #define AUXILIARY_H
 
 #include <iostream>
+#include <vector>
 
 class Aux_link;
 
 class Aux_node
 {
 public:
-    int id;
+    enum Aux_node_type{
+        adding_node,
+        dropping_node,
+        transmitting_node,
+        receiving_node,
+        virtual_transmitting_node,
+        virtual_receiving_node
+    };
+
+    Aux_node_type type;
+    int id; // auxiliary node id
+    int phy_id; // the physical node which this belongs to
+
     Aux_link *first_in, *last_in, *first_out, *last_out;
 
-    Aux_node(int id);
+    Aux_node(int id, int phy_id, Aux_node_type type);
     virtual ~Aux_node();
 };
 
 class Aux_link
 {
 public:
+    enum Aux_link_type{
+        candidate_trail_link,
+        spectrum_link,
+        grooming_link,
+        adding_link,
+        dropping_link,
+        pass_through_link,
+        virtual_adding_link,
+        virtual_dropping_link,
+    };
+
+    Aux_link_type type;
+    double weight;
+
     Aux_node *from, *to;
     Aux_link *prev_same_from, *next_same_from, *prev_same_to, *next_same_to;
-    float cost;
 
-    Aux_link(Aux_node* from, Aux_node* to, float cost);
+    Aux_link(Aux_node* from, Aux_node* to, double weight, Aux_link_type type);
     virtual ~Aux_link();
 };
 
 class Aux_graph
 {
 public:
+    std::vector<Aux_node*> aux_node;
 
-    Aux_graph();
+    Aux_graph(int num_phy_node, int num_slots);
     virtual ~Aux_graph();
 };
+
 #endif /* AUXILIARY_H */
