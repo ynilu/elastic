8f1320443e03443bd0876caa1bac8b95e1ddb737
alanwanga <alanwanga@yahoo.com.tw>
2016-06-20 23:00:40 +0800

    return candidate OTDM path

--- light_path.hpp
+++ light_path.hpp
@@ -20,12 +20,12 @@ public:
     LightPath_type type;
     std::set<int> requests;          // store request_id
     std::list<Aux_link*> aux_link_list;
-    int bandwidth;                   // used_bandwidth
+    int modulation_level;
+    double used_bandwidth;           // used_bandwidth
     double weight;
     Spectrum spectrum;
     Path p_path;                     // store phy_node_id
     std::vector<int> a_nodes;        // store aux_node_id
-
 
     LightPath();
     virtual ~LightPath();
